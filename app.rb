8507b49286f2d969ae5ce9f2e1dcca18fae288e9
Olivia Cubela <oliviacubela@gmail.com>
2020-09-22 08:21:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,18 +24,18 @@ class App < Sinatra::Base
     erb :team
   end
 
-  post '/team' do
-
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
-
-    erb :team
-  end
+  # post '/team' do
+  # 
+  #   @name = params[:name]
+  #   @coach = params[:coach]
+  #   @pg = params[:pg]
+  #   @sg = params[:sg]
+  #   @pf = params[:pf]
+  #   @sf = params[:sf]
+  #   @c = params[:c]
+  # 
+  #   erb :team
+  # end
   # get '/team' do
   #
   #   erb :team
