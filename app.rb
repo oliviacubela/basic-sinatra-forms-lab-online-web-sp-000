62cee05abe74a84e252449747e1d1ff51f816b67
Olivia Cubela <oliviacubela@gmail.com>
2020-09-22 07:58:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ class App < Sinatra::Base
     # puts "Small Forward: #{params[:sf]}"
     # puts "Power Forward: #{params[:pf]}"
     # puts "Center: #{params[:c]}"
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
+    # @name = params[:name]
+    # @coach = params[:coach]
+    # @pg = params[:pg]
+    # @sg = params[:sg]
+    # @pf = params[:pf]
+    # @sf = params[:sf]
+    # @c = params[:c]
     erb :team
   end
 
@@ -30,7 +30,7 @@ class App < Sinatra::Base
   # end
 
   post '/team' do
-    
+
     @name = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
