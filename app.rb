11c44fdb103724ecb44b043d93c02b8f2770b875
Olivia Cubela <oliviacubela@gmail.com>
2020-09-22 08:15:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,24 @@ class App < Sinatra::Base
     erb :newteam       #APP GET/newteam renders basketball team form (DO NOT TOUCH THIS!!)
   end
 
+  post '/newteam' do
+    # puts "Team Name: #{params[:name]}"
+    # puts "Coach: #{params[:coach]}"
+    # puts "Point Guard: #{params[:pg]}"
+    # puts "Shooting Guard: #{params[:sg]}"
+    # puts "Small Forward: #{params[:sf]}"
+    # puts "Power Forward: #{params[:pf]}"
+    # puts "Center: #{params[:c]}"
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
+
   post '/team' do
 
     @name = params[:name]
@@ -18,25 +36,6 @@ class App < Sinatra::Base
 
     erb :team
   end
-
-  post '/newteam' do
-    # puts "Team Name: #{params[:name]}"
-    # puts "Coach: #{params[:coach]}"
-    # puts "Point Guard: #{params[:pg]}"
-    # puts "Shooting Guard: #{params[:sg]}"
-    # puts "Small Forward: #{params[:sf]}"
-    # puts "Power Forward: #{params[:pf]}"
-    # puts "Center: #{params[:c]}"
-    # @name = params[:name]
-    # @coach = params[:coach]
-    # @pg = params[:pg]
-    # @sg = params[:sg]
-    # @pf = params[:pf]
-    # @sf = params[:sf]
-    # @c = params[:c]
-    erb :team
-  end
-
   # get '/team' do
   #
   #   erb :team
